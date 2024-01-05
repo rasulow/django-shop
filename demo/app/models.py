71a461b4d6e75f96fb1a0b9d2a0db4ccc1bd559c
rasulow <rasulov.olympusss@gmail.com>
2024-01-05 22:50:14 +0500

    image upload

--- demo/app/models.py
+++ demo/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,4 +15,18 @@ class Category(models.Model):
         return self.name
 
 
+        
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
+    
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        
         
--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path("__reload__/", include("django_browser_reload.urls"))
-]
+] + static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
